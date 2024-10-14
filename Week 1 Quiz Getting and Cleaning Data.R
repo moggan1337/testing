51d071992bee4a526212d56bccbce96ea9cde776
moggan1337 <155393716+moggan1337@users.noreply.github.com>
2024-10-14 21:38:06 +0200

    Delete Week 1 Quiz Getting and Cleaning Data.R

--- Week 1 Quiz Getting and Cleaning Data.R
+++
@@ -1,8 +0,0 @@
-# Load the data
-data <- read.csv("C:/Users/moogg/Documents/JHU Projects/testing/getdata_data_ss06hid.csv")
-
-# Count the number of properties worth $1,000,000 or more
-million_dollar_properties <- sum(data$VAL == 24, na.rm = TRUE)
-
-# Print the result
-print(million_dollar_properties)
